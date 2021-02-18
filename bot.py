49951298a54082ccb72e4ef087cc6748429849d4
jfcodejf <jacob.friedman@student.uantwerpen.be>
2021-02-18 11:14:07 +0100

    Use strings instead of floats
    also make %load and %claim only accepted in start of string

--- bot.py
+++ bot.py
@@ -15,34 +15,24 @@ async def on_message(message):
     if message.author.bot:
         return
     if message.channel.id in CHANNELS:
-        if "load " in message.content:
+        if message.content.startswith("%load "):
             exercises.clear()
-            data: string = message.content.replace("load ", "")
-            try:
-                for s in [float(s) for s in data.split("; ")]:
-                    exercises.append(s)
-            except:
-                return
-            # shuffle(exercises)
+            data: str = message.content.replace("%load ", "")
+            exercises = data.split("; ")
             print(exercises)
             await message.delete()
             reply = await message.channel.send(
                 "De oefeningen zijn ingelezen! Klik op onderstaande emoji om een oefeningen toegewezen te krijgen of typ '%claim <oefeningnr>'.")
             await reply.add_reaction("\N{THUMBS UP SIGN}")
-            await message.channel.send("Resterende oefeningen: " + ' | '.join([str(e) for e in exercises]))
-        if "claim" in message.content and "%" == message.content[0]:
-            try:
-                nr = float(message.content.replace("claim ", "").replace("=", ""))
-            except:
-                await message.author.send(message.content.replace("claim ", "") + " is geen geldig nummer!")
-                await message.delete()
-                return
-            if not nr in exercises:
-                await message.author.send("Deze oefeningen is al door iemand anders geclaimed!")
+            await message.channel.send("Resterende oefeningen: " + ' | '.join([e for e in exercises]))
+        if message.content.startswith("%claim "):
+            ex = message.content.replace("%claim ", "")
+            if not ex in exercises:
+                await message.author.send("Deze oefeningen is niet beschikbaar!")
             else:
-                exercises.remove(nr)
-                await message.author.send("Je hebt oefening " + str(nr) + " geclaimed. Veel succes!")
-                await message.channel.send("Resterende oefeningen: " + ' | '.join([str(e) for e in exercises]))
+                exercises.remove(ex)
+                await message.author.send("Je hebt oefening " + ex + " geclaimed. Veel succes!")
+                await message.channel.send("Resterende oefeningen: " + ' | '.join([e for e in exercises]))
             await message.delete()
 
 
@@ -54,9 +44,9 @@ async def on_reaction_add(reaction, user):
         return
 
     if emoji == "\N{THUMBS UP SIGN}":
-        oefening: float = exercises.pop(0)
-        await reaction.message.channel.send("Resterende oefeningen: " + ' | '.join([str(e) for e in exercises]))
-        await user.send("Je kan volgende oefening maken: " + str(oefening))
+        oefening: str = exercises.pop(0)
+        await reaction.message.channel.send("Resterende oefeningen: " + ' | '.join([e for e in exercises]))
+        await user.send("Je kan volgende oefening maken: " + oefening)
         await user.send("Veel succes!")
 
     return
