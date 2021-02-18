c435c97659dcb4ac4c7a4295a971725819b0cc32
jfcodejf <jacob.friedman@student.uantwerpen.be>
2021-02-18 11:33:01 +0100

    Merge

--- bot.py
+++ bot.py
@@ -8,6 +8,9 @@ client = discord.Client()
 exercises: list = []
 statusmessage = None
 CHANNELS = [810946686728929300, 810920527736340550, 541408928408272938]
+prefix = "%"
+load_str = prefix + "load "
+claim_str = prefix + "claim "
 
 
 @client.event
@@ -15,20 +18,20 @@ async def on_message(message):
     if message.author.bot:
         return
     if message.channel.id in CHANNELS:
-        if message.content.startswith("%load "):
+        if message.content.startswith(load_str):
             exercises.clear()
-            data: str = message.content.replace("%load ", "")
+            data: str = message.content.replace(load_str, "")
             exercises = data.split("; ")
             print(exercises)
             await message.delete()
             reply = await message.channel.send(
-                "De oefeningen zijn ingelezen! Klik op onderstaande emoji om een oefeningen toegewezen te krijgen of typ '%claim <oefeningnr>'.")
+                "De oefeningen zijn ingelezen! Klik op onderstaande emoji om een oefeningen toegewezen te krijgen of typ " + claim_str + "<oefeningnr>")
             await reply.add_reaction("\N{THUMBS UP SIGN}")
             await message.channel.send("Resterende oefeningen: " + ' | '.join([e for e in exercises]))
-        if message.content.startswith("%claim "):
-            ex = message.content.replace("%claim ", "")
+        if message.content.startswith(claim_str):
+            ex = message.content.replace(claim_str, "")
             if not ex in exercises:
-                await message.author.send("Deze oefeningen is niet beschikbaar!")
+                await message.author.send("Deze oefeningen bestaat niet of is al door iemand anders geclaimed!")
             else:
                 exercises.remove(ex)
                 await message.author.send("Je hebt oefening " + ex + " geclaimed. Veel succes!")
